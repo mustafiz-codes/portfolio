c020292bccbde6a1423a426f5904fe9adaca3185
mustafiz-codes <js.mustafiz@gmail.com>
2020-12-25 14:52:27 +0600

    git pages bug fixed

--- src/App.js
+++ src/App.js
@@ -1,50 +1,44 @@
 import React from "react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-
-import Header from './Header';
-import Home from './Home';
+import Header from "./Header";
+import Home from "./Home";
 import About from "./About";
 import Skills from "./Skills";
 import "./css/app.css";
 import Works from "./Works";
 
-
 function App() {
   return (
     <Router>
       <div className="app">
-      <Header/>
-
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/home">
+            <Header />
             <Home />
           </Route>
           <Route path="/about">
+            <Header />
             <About />
           </Route>
           <Route path="/skills">
+            <Header />
             <Skills />
           </Route>
           <Route path="/works">
+            <Header />
             <Works />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
       </div>
     </Router>
-
   );
 }
 
