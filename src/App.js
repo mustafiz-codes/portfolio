496f961a8b5d3dcb8983d9449392d1171f3071a3
mustafiz-codes <js.mustafiz@gmail.com>
2020-12-25 15:19:05 +0600

    git pages bug

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Header from "./Header";
 import Home from "./Home";
 import About from "./About";
 import Skills from "./Skills";
-import "./css/app.css";
 import Works from "./Works";
+import "./css/app.css";
 
 function App() {
   return (
     <Router>
       <div className="app">
+
+      <Header />
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
@@ -21,19 +23,15 @@ function App() {
             <Home />
           </Route> */}
           <Route path="/about">
-            <Header />
             <About />
           </Route>
           <Route path="/skills">
-            <Header />
             <Skills />
           </Route>
           <Route path="/works">
-            <Header />
             <Works />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
